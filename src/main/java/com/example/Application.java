aaf65fa5bd81081fee19470603ad40736d5038c9
YADLAPALLI VENKATA LALITH KUMAR <54011175+lalith050101@users.noreply.github.com>
2021-04-13 01:01:30 +0530

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = UserModelRepository.class)
+//@EnableJpaRepositories(basePackageClasses = UserModelRepository.class)
 public class Application {
 
 	public static void main(String[] args) {
