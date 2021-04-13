e847bb602833792fdef51b205332e56d7da9f62e
YADLAPALLI VENKATA LALITH KUMAR <54011175+lalith050101@users.noreply.github.com>
2021-04-13 16:47:43 +0530

    Update HomeController.java

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 //@CrossOrigin(origins = "http://localhost:8081")
 //@CrossOrigin(origins = "https://8081-dbdedffdadadeeffdaabdfaccfeebafecbf.examlyiopb.examly.io")
+@CrossOrigin(origins = "https://medworld.daranip.com")
 @RestController
 @RequestMapping("/")
 public class HomeController {
